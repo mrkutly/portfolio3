2964375657360fb23e7c30f5a51c2c04c3c0c543
mark sauer-utley <mark.utley.713@gmail.com>
2020-03-07 13:10:55 -0500

    fix broken dev server imports

--- public/js/contact.js
+++ public/js/contact.js
@@ -1,60 +1,63 @@
+import lottie from 'lottie-web'
+import faceData from '../res/animations/face.json'
+
 window.addEventListener("DOMContentLoaded", () => {
-	const clipboard = ClipboardJS && new ClipboardJS("#email")
-	const face = document.querySelector("#face")
-	const contactMessage = document.querySelector(".contact-message")
-	const darkModeButton = document.querySelector(".mode-toggle")
+  const clipboard = ClipboardJS && new ClipboardJS("#email");
+  const face = document.querySelector("#face");
+  const contactMessage = document.querySelector(".contact-message");
+  const darkModeButton = document.querySelector(".mode-toggle");
 
-	let darkModeStyles = document.querySelector("style#dark")
-	let darkMode = document.cookie === "dark=true"
+  let darkModeStyles = document.querySelector("style#dark");
+  let darkMode = document.cookie === "dark=true";
 
-	applyEventListener(darkModeButton, toggleDarkMode)
+  applyEventListener(darkModeButton, toggleDarkMode);
 
-	clipboard.on("success", () => showSuccessMessage(contactMessage))
-	clipboard.on("error", () => showFailureMessage(contactMessage))
+  clipboard.on("success", () => showSuccessMessage(contactMessage));
+  clipboard.on("error", () => showFailureMessage(contactMessage));
 
-	if (lottie) {
-		const animationOptions = {
-			container: face,
-			path: "/res/animations/face.json",
-			loop: true,
-			renderer: "svg",
-			autoplay: true,
-		}
-		window.anim = lottie.loadAnimation(animationOptions)
-	}
+  if (lottie) {
+    const animationOptions = {
+      container: face,
+      animationData: faceData,
+      loop: true,
+      renderer: "svg",
+      autoplay: true
+    };
+    window.anim = lottie.loadAnimation(animationOptions);
+  }
 
-	function applyEventListener(element, cb) {
-		element.addEventListener("click", cb)
-		element.addEventListener("keypress", e => e.which === 13 && cb(e))
-	}
+  function applyEventListener(element, cb) {
+    element.addEventListener("click", cb);
+    element.addEventListener("keypress", e => e.which === 13 && cb(e));
+  }
 
-	function showSuccessMessage(messageElement) {
-		messageElement.innerHTML = `
+  function showSuccessMessage(messageElement) {
+    messageElement.innerHTML = `
 			<p>
 				Great! It's copied to your clipboard. Feel free to send me that email :)
 			</p>
-		`
-	}
+		`;
+  }
 
-	function showFailureMessage(messageElement) {
-		messageElement.innerHTML = `
+  function showFailureMessage(messageElement) {
+    messageElement.innerHTML = `
 			<p>
 				Hmm there seems to be a problem. You might have to copy and paste the old-fashioned way.
 			</p>
-		`
-	}
+		`;
+  }
 
-	function toggleDarkMode() {
-		if (darkMode) {
-			darkMode = false
-			document.cookie = "dark= ; expires = Thu, 01 Jan 1970 00:00:00 GMT"
-			darkModeButton.textContent = "🌞"
-			darkModeStyles.innerHTML = ""
-		} else {
-			darkMode = true
-			document.cookie = "dark=true"
-			darkModeButton.textContent = "🌚"
-			darkModeStyles.innerHTML = `
+  function toggleDarkMode() {
+    if (darkMode) {
+      darkMode = false;
+      document.cookie = "dark=false";
+      darkModeButton.textContent = "🌞";
+      darkModeStyles.innerHTML = "";
+    } else {
+      darkMode = true;
+      document.cookie = "dark=true";
+      darkModeButton.textContent = "🌚";
+      darkModeStyles.innerHTML = `
             body {
                background-color: #292929;
                color: white;
@@ -66,7 +69,7 @@ window.addEventListener("DOMContentLoaded", () => {
                fill: rgb(255, 255, 255);
                stroke: rgb(255, 255, 255);
             } 
-         `
-		}
-	}
-})
+         `;
+    }
+  }
+});
--- public/js/index.js
+++ public/js/index.js
@@ -1,48 +1,53 @@
+import lottie from 'lottie-web';
+import faceData from '../res/animations/face.json';
+import skillsData from '../res/animations/skills.json';
+import workData from '../res/animations/work.json';
+
 window.addEventListener("DOMContentLoaded", () => {
-	const header = document.querySelector("header");
-	const face = document.getElementById("face");
-	const skillsAnimation = document.getElementById("skills-animation");
-	const skillsGrid = document.querySelector(".skills-grid");
-	const workAnimation = document.getElementById("work-animation");
-	const workGrid = document.querySelector(".work-grid");
-	const darkModeButton = document.querySelector(".mode-toggle");
-	const revealElements = document.querySelectorAll(".reveal");
-	const observableElements = [...revealElements, header, skillsGrid, workGrid];
+  const header = document.querySelector("header");
+  const face = document.getElementById("face");
+  const skillsAnimation = document.getElementById("skills-animation");
+  const skillsGrid = document.querySelector(".skills-grid");
+  const workAnimation = document.getElementById("work-animation");
+  const workGrid = document.querySelector(".work-grid");
+  const darkModeButton = document.querySelector(".mode-toggle");
+  const revealElements = document.querySelectorAll(".reveal");
+  const observableElements = [...revealElements, header, skillsGrid, workGrid];
 
-	let darkModeStyles = document.querySelector("style#dark");
-	let darkMode = document.cookie === "dark=true";
+  let darkModeStyles = document.querySelector("style#dark");
+  let darkMode = document.cookie === "dark=true";
 
-	applyEventListener(darkModeButton, toggleDarkMode);
+  applyEventListener(darkModeButton, toggleDarkMode);
 
-	const options = { threshold: 0 };
-	const observer = new IntersectionObserver(
-		entries => entries.map(observerSwitch),
-		options
-	);
+  const options = { threshold: 0 };
+  const observer = new IntersectionObserver(
+    entries => entries.map(observerSwitch),
+    options
+  );
 
-	observableElements.forEach(el => observer.observe(el));
+  observableElements.forEach(el => observer.observe(el));
 
-	if (lottie) {
-		const animationOptions = {
-			container: face,
-			path: "res/animations/face.json",
-			loop: true,
-			renderer: "svg",
-			autoplay: true,
-		};
-		lottie.loadAnimation(animationOptions);
-	}
+  if (lottie) {
+    const animationOptions = {
+      container: face,
+      animationData: faceData,
+      loop: true,
+      renderer: "svg",
+      autoplay: true
+    };
+    lottie.loadAnimation(animationOptions);
+  }
 
-	function toggleDarkMode() {
-		if (darkMode) {
-			darkMode = false;
-			document.cookie = "dark= ; expires = Thu, 01 Jan 1970 00:00:00 GMT";
-			darkModeStyles.innerHTML = "";
-			darkModeButton.textContent = "🌞";
-		} else {
-			darkMode = true;
-			document.cookie = "dark=true";
-			darkModeStyles.innerHTML = `
+  function toggleDarkMode() {
+    if (darkMode) {
+      darkMode = false;
+      document.cookie = "dark=false";
+      darkModeStyles.innerHTML = "";
+      darkModeButton.textContent = "🌞";
+    } else {
+      darkMode = true;
+      document.cookie = "dark=true";
+      darkModeStyles.innerHTML = `
             body {
                background-color: #292929;
                color: white;
@@ -58,58 +63,58 @@ window.addEventListener("DOMContentLoaded", () => {
 					color: #231f1f;
 				}
 			`;
-			darkModeButton.textContent = "🌚";
-		}
-	}
+      darkModeButton.textContent = "🌚";
+    }
+  }
 
-	function applyEventListener(element, cb) {
-		element.addEventListener("click", cb);
-		element.addEventListener("keypress", e => e.which === 13 && cb(e));
-	}
+  function applyEventListener(element, cb) {
+    element.addEventListener("click", cb);
+    element.addEventListener("keypress", e => e.which === 13 && cb(e));
+  }
 
-	function observerSwitch(entry) {
-		switch (entry.target) {
-			case header:
-				if (entry.isIntersecting) {
-					face.classList.remove("stuck");
-				} else {
-					face.classList.add("stuck");
-				}
-				break;
+  function observerSwitch(entry) {
+    switch (entry.target) {
+      case header:
+        if (entry.isIntersecting) {
+          face.classList.remove("stuck");
+        } else {
+          face.classList.add("stuck");
+        }
+        break;
 
-			case skillsGrid:
-				if (entry.isIntersecting && lottie) {
-					const animationOptions = {
-						container: skillsAnimation,
-						path: "/res/animations/skills.json",
-						loop: false,
-						renderer: "svg",
-						autoplay: true,
-					};
-					lottie.loadAnimation(animationOptions);
-					observer.unobserve(entry.target);
-				}
-				break;
+      case skillsGrid:
+        if (entry.isIntersecting && lottie) {
+          const animationOptions = {
+            container: skillsAnimation,
+            animationData: skillsData,
+            loop: false,
+            renderer: "svg",
+            autoplay: true
+          };
+          lottie.loadAnimation(animationOptions);
+          observer.unobserve(entry.target);
+        }
+        break;
 
-			case workGrid:
-				if (entry.isIntersecting && lottie) {
-					const animationOptions = {
-						container: workAnimation,
-						path: "/res/animations/work.json",
-						loop: false,
-						renderer: "svg",
-						autoplay: true,
-					};
-					lottie.loadAnimation(animationOptions);
-					observer.unobserve(entry.target);
-				}
-				break;
+      case workGrid:
+        if (entry.isIntersecting && lottie) {
+          const animationOptions = {
+            container: workAnimation,
+            animationData: workData,
+            loop: false,
+            renderer: "svg",
+            autoplay: true
+          };
+          lottie.loadAnimation(animationOptions);
+          observer.unobserve(entry.target);
+        }
+        break;
 
-			default:
-				if (entry.isIntersecting) {
-					entry.target.classList.add("revealed");
-					observer.unobserve(entry.target);
-				}
-		}
-	}
+      default:
+        if (entry.isIntersecting) {
+          entry.target.classList.add("revealed");
+          observer.unobserve(entry.target);
+        }
+    }
+  }
 });
